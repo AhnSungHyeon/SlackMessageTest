a111518adcab950ac002c2ed2e562d51afa553ae
META_ASH\sh729 <ash7299@metaconnection.kr>
2023-10-17 10:32:51 +0900

    깃헙 커밋 메시지 조회

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Octokit } from "@octokit/rest";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [commitMessage, setCommitMessage] = useState();
+
+  useEffect(() => {
+    const githubToken = import.meta.env.VITE_GITHUB_TOKEN;
+    const fetchLatestCommit = async () => {
+      const octokit = new Octokit({
+        auth: githubToken,
+      });
+
+      try {
+        const { data } = await octokit.request(
+          "GET /repos/{owner}/{repo}/commits/{ref}",
+          {
+            owner: "AhnSungHyeon", // 저장소의 소유자 이름을 넣어주세요.
+            repo: "SlackMessageTest", // 저장소의 이름을 넣어주세요.
+            ref: "main", // 대상 브랜치 이름 (예: 'main' 또는 'master')
+          }
+        );
+        console.log(data);
+      } catch (error) {
+        console.error("Error fetching commit:", error);
+      }
+    };
+
+    fetchLatestCommit();
+  }, []);
 
   return (
     <>
